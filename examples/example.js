8a20fb44ae6f9acb44b60dd4993d2aee9ddc83df
James Nocentini <james.nocentini@gmail.com>
2016-07-27 14:56:56 +0100

    Update example/example.js

--- examples/example.js
+++ examples/example.js
@@ -1,11 +1,36 @@
 // Load bootprint
 require('bootprint')
-  // Load bootprint-swagger
+// Load bootprint-swagger
   .load(require('../'))
   // Customize configuration, override any options
-  .merge({ /* Any other configuration */})
+  .merge({/* Any other configuration */})
   // Specify build source and target
-  .build('./../../public.yaml', './../../../site/gen-preview/develop/references/sync-gateway/rest-api')
+  .build('./../../tmp/admin.json', './../../../site/gen-hippo/develop/references/sync-gateway/admin-rest-api')
   // Generate swagger-documentation into "target" directory
   .generate()
-  .done(console.log)
+  .done(console.log);
+
+
+// Load bootprint
+require('bootprint')
+// Load bootprint-swagger
+  .load(require('../'))
+  // Customize configuration, override any options
+  .merge({/* Any other configuration */})
+  // Specify build source and target
+  .build('./../../tmp/public.json', './../../../site/gen-hippo/develop/references/sync-gateway/rest-api')
+  // Generate swagger-documentation into "target" directory
+  .generate()
+  .done(console.log);
+
+// Load bootprint
+require('bootprint')
+// Load bootprint-swagger
+  .load(require('../'))
+  // Customize configuration, override any options
+  .merge({/* Any other configuration */})
+  // Specify build source and target
+  .build('./../../tmp/lite.json', './../../../site/gen-hippo/develop/references/couchbase-lite/rest-api')
+  // Generate swagger-documentation into "target" directory
+  .generate()
+  .done(console.log);
